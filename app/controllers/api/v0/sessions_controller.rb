7073907a04d7b1eaff7c632e3b2a4b8f27f19458
Captainlearyo <clayoleary@gmail.com>
2024-01-15 17:53:31 -0600

    tweek

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V0::SessionsController < ApplicationController
   def create
-    @json_response = JSON.parse(request.body.read, symbolize_names: true)
-    @user = User.find_by(email: @json_response[:email])
-    if @user.authenticate(@json_response[:password])
+    json_response = JSON.parse(request.body.read, symbolize_names: true)
+    user = User.find_by(email: json_response[:email])
+    if @user.authenticate(json_response[:password])
       render json: UserSerializer.new(@user)
     else
       render json: { error: "User name or password incorrect" }, status: :not_found
